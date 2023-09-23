98b0c2f28d07d413901fd37d53622baa84342198
Esraa Abduallah <esraahisham753@gmail.com>
2023-09-23 16:46:45 +0000

    Add full functional pagination

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,6 +1,49 @@
+import React from "react";
+export default class Pagination {
+  constructor(data, pageSize) {
+    this.data = data;
+    this.pageSize = pageSize;
+  }
 
-export default class Pagination  {
-    constructor(data, pageSize) {
-        
-    }
+  getPage(n) {
+    const offset = n * this.pageSize;
+    console.log(
+      "Data from getPage: ",
+      this.data.slice(offset, offset + this.pageSize).length,
+    );
+    return this.data.slice(offset, offset + this.pageSize);
+  }
+
+  getTotalPages() {
+    console.log("Total pages: ", Math.ceil(this.data.length / this.pageSize));
+    return Math.ceil(this.data.length / this.pageSize);
+  }
+
+  getUpToPage(n) {
+    const offset = n * this.pageSize;
+    return this.data.slice(0, offset + this.pageSize);
+  }
 }
+
+export const PageNavigation = ({
+  nextPageHandler,
+  previousPageHandler,
+  currentPage,
+  totalPages,
+}) => {
+  return (
+    <div className="page-navigation">
+      {currentPage === 0 ? null : (
+        <button onClick={previousPageHandler}>◀️ Prev</button>
+      )}
+
+      {currentPage + 1 === totalPages ? null : (
+        <button onClick={nextPageHandler}>Next ▶️</button>
+      )}
+
+      <span>
+        {currentPage + 1} of {totalPages}
+      </span>
+    </div>
+  );
+};
